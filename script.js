feb63c6678ad53472beed02074cc5f756ec32407
Sajal Baral <sajalsbaral@gmail.com>
2025-05-06 18:12:07 -0400

    updated local storage and added dynamic book rendering

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const modal = document.getElementById("modal");
 const insideModal = document.querySelector(".modal-inner");
 const submit = document.getElementById("submit");
 
+let myLibrary = [];
+
 //opens add book form
 openBtn.addEventListener("click", () => {
   modal.classList.add("open");
@@ -18,6 +20,7 @@ modal.addEventListener("click", (event) => {
 //Book class
 class Book {
   constructor(title, author, pages, isRead) {
+    this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -29,47 +32,57 @@ class Book {
   }
 }
 
-//Adds a book to the library
 function addBookToLib(book) {
-  const bookContainer = document.querySelector(".book-grid-container");
-
-  const bookGrid = document.createElement("div");
-  bookGrid.classList.add("bookGrid");
-
-  const titleP = document.createElement("p");
-  titleP.innerText = book.title;
-
-  const authorP = document.createElement("p");
-  authorP.innerText = book.author;
-
-  const pagesP = document.createElement("p");
-  pagesP.innerText = book.pages;
-
-  const readButton = document.createElement("button");
-  readButton.classList.add("button");
-  readButton.innerText = book.isRead ? "Read" : "Not Read";
-  readButton.classList.add(book.isRead ? "buttonRead" : "notRead");
-  readButton.addEventListener("click", () => {
-    book.isRead = !book.isRead;
-    readButton.innerText = book.isRead ? "Read" : "Not Read";
-    readButton.classList.toggle("buttonRead");
-    readButton.classList.toggle("notRead");
-  });
-
-  const removeButton = document.createElement("button");
-  removeButton.classList.add("button");
-  removeButton.innerText = "Remove";
-  removeButton.classList.add("buttonRemove");
-  removeButton.classList.add("button");
-  removeButton.addEventListener("click", () => {
-    bookGrid.remove();
-    updateLocalStorage();
-  });
-
-  bookGrid.append(titleP, authorP, pagesP, readButton, removeButton);
-  bookContainer.appendChild(bookGrid);
-
+  myLibrary.push(book);
   updateLocalStorage();
+  renderLibrary();
+}
+
+//Adds a book to the library
+function renderLibrary() {
+  const bookContainer = document.querySelector(".book-grid-container");
+  bookContainer.innerHTML = "";
+
+  myLibrary.forEach((book, index) => {
+    const bookGrid = document.createElement("div");
+    bookGrid.classList.add("bookGrid");
+
+    const titleP = document.createElement("p");
+    titleP.innerText = book.title;
+
+    const authorP = document.createElement("p");
+    authorP.innerText = book.author;
+
+    const pagesP = document.createElement("p");
+    pagesP.innerText = book.pages;
+
+    const readButton = document.createElement("button");
+    readButton.classList.add("button");
+    readButton.innerText = book.isRead ? "Read" : "Not Read";
+    readButton.classList.add(book.isRead ? "buttonRead" : "notRead");
+    readButton.addEventListener("click", () => {
+      book.isRead = !book.isRead;
+      readButton.innerText = book.isRead ? "Read" : "Not Read";
+      readButton.classList.toggle("buttonRead");
+      readButton.classList.toggle("notRead");
+      updateLocalStorage();
+      renderLibrary();
+    });
+
+    const removeButton = document.createElement("button");
+    removeButton.classList.add("button");
+    removeButton.innerText = "Remove";
+    removeButton.classList.add("buttonRemove");
+    removeButton.classList.add("button");
+    removeButton.addEventListener("click", () => {
+      myLibrary.splice(index, 1);
+      updateLocalStorage();
+      renderLibrary();
+    });
+
+    bookGrid.append(titleP, authorP, pagesP, readButton, removeButton);
+    bookContainer.appendChild(bookGrid);
+  });
 }
 
 //grabs values once submit is clicked
@@ -97,20 +110,8 @@ submit.addEventListener("click", () => {
 
 //Updates localStorage with current books so it doesn't disappear if refreshed
 function updateLocalStorage() {
-  const books = [];
-
-  // Collect all the books from the page
-  document.querySelectorAll(".bookGrid").forEach((bookGrid) => {
-    const title = bookGrid.querySelector("p:nth-child(1)").innerText;
-    const author = bookGrid.querySelector("p:nth-child(2)").innerText;
-    const pages = bookGrid.querySelector("p:nth-child(3)").innerText;
-    const isRead = bookGrid.querySelector("button.buttonRead") ? true : false;
-
-    books.push({ title, author, pages, isRead });
-  });
-
   // Save to localStorage
-  localStorage.setItem("books", JSON.stringify(books));
+  localStorage.setItem("books", JSON.stringify(myLibrary));
 }
 
 //Load books from localStorage
@@ -118,14 +119,15 @@ window.addEventListener("DOMContentLoaded", () => {
   const savedBooks = JSON.parse(localStorage.getItem("books"));
 
   if (savedBooks) {
-    savedBooks.forEach((bookData) => {
-      const book = new Book(
-        bookData.title,
-        bookData.author,
-        bookData.pages,
-        bookData.isRead
-      );
-      addBookToLib(book);
-    });
+    myLibrary = savedBooks.map(
+      (bookData) =>
+        new Book(
+          bookData.title,
+          bookData.author,
+          bookData.pages,
+          bookData.isRead
+        )
+    );
+    renderLibrary();
   }
 });
